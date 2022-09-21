f1542b4d904ff6670b78002caef76133da397f47
KacperD123 <113014114+KacperD123@users.noreply.github.com>
2022-09-21 09:47:32 +0200

    Add files via upload

---
+++ Firstproject.cpp
@@ -0,0 +1,51 @@
+// Firstproject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Komentarz
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello  World!\n";
+    std::cout << "Predkosc to km/h";
+
+    /*
+    Instrukcja do wyœwietlania informacji na standardowy strumieñ wyjœcia.
+    Domyœlnie, jesli nie zosta³o to zmienione, jest to konsola w której aplikacja
+    zosta³a uruchomiona.
+    Aby wyswietliæ na konsoli nalezy u¿yæ std::cout.
+    Na konosle wywy³amy za pomoca operatora <<.
+    Po nim musi znajdowaæ siê dana do wys³ania.
+    Instrukcjê koñczymy znakiem œrednik.
+    "Hello World!\n" - jest to sta³a o wartoœci tekstowej.
+    Sta³e tekstowe s¹ ograniczone znakami cudzys³owu.
+    * ' (apostrof) - jesli jest jeden znak
+    * " (cudzys³ów) - jesli jest zero lub wiêcej znaków
+    W sta³ych tekstowych moga byæ wstawiane znaki specjalne. Takie znaki
+    zaczynaj¹ siê od znaku \ (backslash) po którym mo¿e siê znajdowaæ:
+    * jedna litera alfabetu angielskiego
+    * trzy cyfry daj¹ce liczbê ósemkow¹
+    Przyk³adowo:
+    * \n - symbol oznaczaj¹cy now¹ liniê.
+    * \t - symbol oznaczaj¹cy znak tabulatora
+    * \\ - symbol oznaczaj¹cy znak \
+    * \' - symbol oznaczaj¹cy znak '
+    * \" - symbol oznaczaj¹cy znak "
+    * \141 - symbol ósemkowy oznaczajacy zgodnie z tablic¹ kodów ASCII znak m³aej literki a
+    */
+
+    //github.com/IronLittleFox
+
+    std::cout << "Hel\nlo World!\n";
+    std::cout << "Hello World!\n";
+
+    std::cout << "Predkosc to km\\h";
+
+    std::cout << "Tabulator\ttabulator";
+    std::cout << 56;
+    std::cout << "\n";
+    std::cout << 36;
+    std::cout << "\n";
+    std::cout << 136;
+
+}
+
